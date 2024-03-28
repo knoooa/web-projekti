f78a38a5334633402dea4dbd885b41dab65e44fd
knoooa <144010719+knoooa@users.noreply.github.com>
2024-03-28 11:02:26 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from os import getenv
+import secrets
+
+app = Flask(__name__)
+
+#app.secret_key = getenv("SECRET_KEY")
+app.secret_key = secrets.token_hex(16)
+
+import routes
+    
