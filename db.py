e6b7ac216be7f1efc45bd88f879ca2b93a9c42e5
Nooa <nooa.kosteila@gmail.com>
2024-04-06 21:18:15 +0300

    modify db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:/uuseri:salainensana@localhost/chat"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:/USERNAME:PASSWORD@localhost/chat"
 
 db = SQLAlchemy(app)
 
